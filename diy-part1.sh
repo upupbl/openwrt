d963386fc1a0789f977eeafc095ac59ccd8c4b81
upupbl <119369292+upupbl@users.noreply.github.com>
2023-03-14 08:34:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+ See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
